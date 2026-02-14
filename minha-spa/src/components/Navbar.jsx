ae9f4230367a4ed3357d4cc25bbf8c54c4cde585
JV Alves <joao-vitor1142@hotmail.com>
2026-02-13 21:25:11 -0300

    aula do dia 13/02

--- minha-spa/src/components/Navbar.jsx
+++ minha-spa/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import {Link} from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
 
 //Componentes de navegação -reutilizáveis
 //Criar barra de navegação
 export function Navbar() {
   return (
-      <nav style={{padding: '1rem', borderBottom: '1px solid #ccc'}}>
-        <Link to="/" style={{ marginRight: '10px'}}>Home</Link>
-        <Link to="/sobre">Sobre</Link>
+      <nav className={styles.navbar}>
+        <Link to="/" className={styles.link}>Home</Link>
+        <Link to="/sobre" className={styles.link}>Sobre</Link>
       </nav>
   );
 }
