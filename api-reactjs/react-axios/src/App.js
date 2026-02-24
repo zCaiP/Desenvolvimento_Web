a3fb9d1e9f5b04720776408f8c6e9bdf14901c00
zCaiP <joao-vitor1142@hotmail.com>
2026-02-23 21:28:26 -0300

    aula do dia 23/02

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -1,28 +1,64 @@
 import React, {useEffect, useState} from "react";
 import api from "./services/api";
+import './App.css';
 
 export default function App() {
+    const [username, setUsername] = useState('zCaiP');
     const [user, setUser] = useState(null);
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     
+
     //carrega dados da API
     useEffect(() =>{
-      async function loadUser(){
+      async function loadData(userToSearch = username){
         try{
-          const response = await api.get('/user');
-          setUser(response.data);
+          setLoading(true);
+          setError(null);
+
+          //carrega dados do usuário
+          const userResponse = await api.get(`/users/${userToSearch}`); //API Responde com os dados do usuário
+          setUser(userResponse.data); // Extrai os dados da API
+
+          const reposReponse = await api.get(`/users/${userToSearch}/repos?per_page=6&sort=updated`);
+          setRepos(reposReponse.data);
+
         } catch(error){
           console.error('Erro', error)
+          setError(`Usuário ${userToSearch} não encontrado`);
+
+        } finally{
+          setLoading(false);
         }
+        loadData();
       }
-      loadUser();
     }, []);
 
-    if (!user) return <div>Carregando...</div>
+    if (loading){
+      return (
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p>Carregando Perfil...</p>
+        </div>
+      );
+    }
+
+    if (error || !user){
+      return (
+        <div className="error-container">
+          <h2> ❌ {error}</h2>
+          <button onClick={ () => window.location.reload()}>Tentar Novamente</button>
+
+        </div>
+      )
+    }
+
 
     return (
-      <div className={App}>
-        <p>Usuário: {user.login}</p>
-        <p>Biografia: {user.bio|| 'Sem biografia'}</p>
+      <div className="app-container">
+        <div className="profile-card"></div>
+
       </div>
       );
 }
