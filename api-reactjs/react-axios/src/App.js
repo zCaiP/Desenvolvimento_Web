aa8d1151d13e5c7ca6943e54e313769947867cc6
zCaiP <joao-vitor1142@hotmail.com>
2026-03-03 21:07:16 -0300

    atualizando aula do dia 03/03

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -3,7 +3,7 @@ import api from "./services/api";
 import './App.css';
 
 export default function App() {
-    const [username, setUsername] = useState('zCaiP');
+    const [username] = useState('zCaiP');
     const[user, setUser] = useState(null);
     const[repos, setRepos] = useState([]);
     const[loading, setLoading] = useState(true);
